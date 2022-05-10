2e7e528ea40a98bfaa26c7dd54e1f5b5e4e7f4ad
Yonathan Dawang Jati <yonathandawang111@gmail.com>
2022-05-10 17:11:32 +0700

    Finalize script.js

---
+++ rectangle-pattern/script.js
@@ -0,0 +1,11 @@
+// Rectangle Pattern
+let sign = ""
+for(a = 0; a <= 4; ++a){
+    for(b = 0; b <= 8; ++b){
+        sign += "*";
+    }
+    sign += "\n";
+}
+console.log(sign);
+
+// End of Rectangle Pattern
